0932d77203061170eff1ac7c41575f5540a87b0a
markcascardi <mcascardi@gmail.com>
2020-04-24 14:40:21 -0400

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,94 @@
+require 'pry'
+
+  class TicTacToe
+
+    WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+
+    def initialize
+      @board = [" "," "," "," "," "," "," "," "," "]
+    end
+
+    def display_board
+      puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+      puts "-----------"
+      puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+      puts "-----------"
+      puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+    end
+
+    def input_to_index(input)
+      input.to_i - 1
+    end
+
+    def move(index, current_player = "X")
+      @board[index] = current_player
+    end
+
+    def position_taken?(index)
+      @board[index] != (" ")
+    end
+
+    def valid_move?(index)
+      index.between?(0,8) && !position_taken?(index)
+    end
+
+    def turn
+      puts "Select a position between 1-9:"
+      input = gets.strip
+      index = input_to_index(input)
+
+      if valid_move?(index)
+        move(index, current_player)
+        display_board
+      else
+        puts "Invalid number, please select again:"
+        turn
+      end
+    end
+
+    def turn_count
+      @board.count { |token| token == "X" || token == "O" }
+    end
+
+    def current_player
+      turn_count % 2 == 0 ? "X" : "O"
+    end
+
+    def won?
+      WIN_COMBINATIONS.detect do |winning_combo|
+        board_combo = winning_combo.map do |index|
+          @board[index]
+        end
+
+        ["XXX", "OOO"].include?(board_combo.join)
+      end
+    end
+
+    def full?
+      @board.all? { |square| square != " " }
+    end
+
+    def draw?
+      @board.all? { |square| square != " " && !won? }
+    end
+
+    def over?
+      draw? || full? || won?
+    end
+
+    def winner
+      if won?
+        @board[won?[0]]
+      end
+    end
+
+    def play
+      turn until over?
+      
+      if won?
+        puts "Congratulations #{winner}!"
+      elsif draw?
+        puts "Cat's Game!"
+      end
+    end
+  end
